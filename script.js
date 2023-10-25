d832d6b84245bcc531fb23e3871015485f3dc5b3
Andrey <kedr_club@mail.ru>
2023-10-25 08:04:47 +0300

    feat: puts the script in a separate file

---
+++ script.js
@@ -0,0 +1,25 @@
+const TOKEN = "6576357682:AAEasTAVcLjZ46ikfLlx--OtG66QinL79IM";
+const CHAT_ID = "-1002069195468";
+const URI_API = `https:\\api.telegram.org/bot${TOKEN}/sendMessage`;
+const main = document.querySelector(".main");
+
+document.querySelector(".form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let message = `<b>Опрос об Илье Муромце</b>\n`;
+  message += `<b>1. </b>${this.hero.value}\n`;
+  message += `<b>2. </b>${this.area.value}\n`;
+  message += `<b>3. </b>${this.murom.value}`;
+
+  axios
+    .post(URI_API, {
+      chat_id: CHAT_ID,
+      parse_mode: "html",
+      text: message,
+    })
+    .then((res) => {
+      main.innerHTML = `<h1 class="title">Спасибо большое!<br>Ответ получен.</h1>`;
+    })
+    .catch((err) => console.warn(err))
+    .finally(() => console.log("Конец"));
+});
